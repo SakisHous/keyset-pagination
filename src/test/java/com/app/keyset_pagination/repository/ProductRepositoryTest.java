f9f1a990860a39415693cc0a51f5baae2c1621d8
thanasis <th.chousiadas@gmail.com>
2026-04-26 23:57:06 +0300

    fix: product repository test

--- src/test/java/com/app/keyset_pagination/repository/ProductRepositoryTest.java
+++ src/test/java/com/app/keyset_pagination/repository/ProductRepositoryTest.java
@@ -6,9 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 
+import java.time.Instant;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class ProductRepositoryTest extends TestContainersIntegration {
@@ -29,16 +30,29 @@ class ProductRepositoryTest extends TestContainersIntegration {
     @Test
     void shouldFindFirstPageByRecommendations() {
 
-        List<Product> firstRowsForReviews = productRepository.findFirstPageByRecommendations(PAGE_SIZE);
+        List<Product> firstRowsForRecommendations = productRepository.findFirstPageByRecommendations(PAGE_SIZE);
 
-        assertEquals(PAGE_SIZE, firstRowsForReviews.size());
+        assertEquals(PAGE_SIZE, firstRowsForRecommendations.size());
     }
 
     @Test
     void shouldFindNextProductPageOrderByCreatedDate() {
+
+        List<Product> nextPage = productRepository.findNextProductPageOrderByCreatedDate(Instant.now(), 10L, PAGE_SIZE);
+
+        assertFalse(nextPage.isEmpty());
+        assertEquals(PAGE_SIZE, nextPage.size());
     }
 
     @Test
     void shouldFindNextProductPageOrderByRecommendations() {
+
+        final double lastRecommendations = 4.5;
+        final long lastId = 10L;
+
+        List<Product> nextPage = productRepository.findNextProductPageOrderByRecommendations(lastRecommendations, lastId, PAGE_SIZE);
+
+        assertFalse(nextPage.isEmpty());
+        assertEquals(PAGE_SIZE, nextPage.size());
     }
 }
